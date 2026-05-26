a3dffdc463a73fc9af337c24e08fb39d0611d6ad
Jasur <sayfullaevjasa22@gmail.com>
2026-05-26 21:39:40 +0500

    fix login

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
 export const config = {
   matcher: [
